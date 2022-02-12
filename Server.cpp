0d392f76f81e75d8537434696977065c8ea31d68
2zumi <zxvc7777@gmail.com>
2022-02-12 18:47:57 +0900

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -311,8 +311,10 @@ void CheckMessage(int userNumber, char receive[], int length)
 				//유저가 있음!
 				if (pollFDArray[i].fd != -1 && userFDArray[i] != nullptr)
 				{
+					char* currentUserMessage = new char[14];
+					memcpy(currentUserMessage, currentMessage, 14);
 					//유저한테 이동 내용을 전달해주기!
-					userFDArray[i]->MessageQueueing(currentMessage);
+					userFDArray[i]->MessageQueueing(currentUserMessage);
 					//write(pollFDArray[i].fd, receive, length - 1);
 				};
 			};
